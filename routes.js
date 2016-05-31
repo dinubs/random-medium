1f01b74493f57539459324e38dbf8886a8466189
Gavin Dinubilo <gavincodes@gmail.com>
2016-05-31 14:52:57 -0700

    add in asset routes

--- routes.js
+++ routes.js
@@ -4,4 +4,20 @@ const c = require('./controllers');
 module.exports = function(server) {
   // Base routes
   server.route({method: 'GET', path: '/{path*}', handler: c.Base.index});
+  
+  // Static files
+  server.route({
+    method: 'GET',
+    path: '/css/{file}.css',
+    handler: function (request, reply) {
+        reply.file("./public/css/"+request.params.file+".css");
+    }
+  });
+  server.route({
+      method: 'GET',
+      path: '/js/{file}.js',
+      handler: function (request, reply) {
+          reply.file("./public/js/"+request.params.file+".js");
+      }
+  });
 };
